d205cb76ff1395712cfebf5d017699bc43fafbfe
Shoucheng H <h.shoucheng@gmail.com>
2017-03-20 18:01:06 +0800

    ind min picture and set it's style.

--- js/index.js
+++ js/index.js
@@ -6,6 +6,37 @@ function imgLocation(parent, content) {
     //将parent下没有图片的 content 全部取出
     var cparent = document.getElementById(parent);
     var ccontent = getChildElement(cparent, content);
+    //得到图片宽度
+    var imgWidth = ccontent[0].offsetWidth;
+    //单个图片的个数
+    var num = Math.floor(document.documentElement.clientWidth / imgWidth);
+    cparent.style.cssText = "width:" + imgWidth * num + "px" + ";margin:0 auto";
+
+    //得到每张图片的高度
+    var BoxHeightArr = [];
+    for (var i = 0; i < ccontent.length; i++) {
+        if (i < num) {
+            BoxHeightArr[i] = ccontent[i].offsetHeight;
+            console.log(BoxHeightArr[i]);
+        } else {
+            var minHeight = Math.min.apply(null, BoxHeightArr);
+            var minIndex = getMinHeightLocation(BoxHeightArr, minHeight);
+
+            ccontent[i].style.position = 'absolute';
+            ccontent[i].style.top = minHeight + "px";
+            ccontent[i].style.left = ccontent[minIndex] + ccontent[i].offsetLeft + 'px';
+        }
+    }
+
+}
+
+//找到最小高度图片的位置
+function getMinHeightLocation(BoxHeightArr, minHeight) {
+    for (var i in BoxHeightArr) {
+        if (BoxHeightArr[i] == minHeight) {
+            return i;
+        }
+    }
 }
 
 function getChildElement(parent, content) {
@@ -17,4 +48,6 @@ function getChildElement(parent, content) {
         }
     }
     return contentArr;
+}
+tArr;
 }
